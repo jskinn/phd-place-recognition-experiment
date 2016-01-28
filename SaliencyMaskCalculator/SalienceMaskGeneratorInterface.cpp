d773097609357b5a0c4fbed9f91e574c8fec5317
John <little.jimmy.alias@gmail.com>
2016-01-28 14:41:30 +1000

    Some renames and new interfaces to handle expanded project scope.

---
+++ SaliencyMaskCalculator/SalienceMaskGeneratorInterface.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "SalienceMaskGeneratorInterface.h"
+
+
+SalienceMaskGeneratorInterface::SalienceMaskGeneratorInterface()
+{
+}
+
+
+SalienceMaskGeneratorInterface::~SalienceMaskGeneratorInterface()
+{
+}
---
+++ SaliencyMaskCalculator/SalienceMaskGeneratorInterface.h
@@ -0,0 +1,8 @@
+#pragma once
+class SalienceMaskGeneratorInterface
+{
+public:
+	SalienceMaskGeneratorInterface();
+	~SalienceMaskGeneratorInterface();
+};
+
